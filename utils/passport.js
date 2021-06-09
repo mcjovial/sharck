b54416a6a1ca101be61f64eec01483236f6fa881
mcjovial <emmanuel.elias.232317@unn.edu.ng>
2021-06-09 18:50:04 -0400

    auth with passport-facebook

--- utils/passport.js
+++ utils/passport.js
@@ -1,5 +1,6 @@
 var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
+var FacebookStrategy = require('passport-facebook').Strategy;
 
 passport.serializeUser(function (user, done) {
   done(null, user._id);
@@ -30,5 +31,41 @@ passport.use(new LocalStrategy({
 
       return done(null, user);
     })
+  }
+));
+
+passport.use(new FacebookStrategy({
+    clientID: '536159177756262',
+    clientSecret: 'a6c08c2d103990c3cb54a040ad4b4622',
+    callbackURL: 'http://localhost:3000/auth/facebook/callback',
+    profileFields: ['id', 'displayName', 'email']
+  },
+  function(token, refreshToken, profile, done) {
+    User.findOne({'facebookId': profile.id}, function(err, user) {
+      if (err) return done(err);
+
+      if (user) {
+        return done(null, user);
+      } else {
+        User.findOne({email: profile.emails[0].value}, function (err, user) {
+          if (user) {
+            user.facebookId = profile.id
+            return user.save(function (err) {
+              if (err) return done(null, false, { message: "Can't save user info"});
+              return done(null, user);
+            })
+          }
+
+          var user = new User();
+          user.name = profile.displayName;
+          user.email = profile.emails[0].value;
+          user.facebookId = profile.id
+          user.save(function (err) {
+            if (err) return done(null, false, { message: "Can't save user info"});
+            return done(null, user);
+          });
+        })
+      }
+    });
   }
 ));
